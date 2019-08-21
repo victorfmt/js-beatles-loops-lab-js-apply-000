ed6384094c39d18620b7da139cf31f6939fb3a89
Victor Torres <victor.fmt@gmail.com>
2019-08-21 20:53:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ function theBeatlesPlay(musicians, instruments) {
   for(var i = 0; i < musicians.length; i++) {
 //     array.push(`${musicians[i]} plays ${instruments[i]}`);
 }
-return array
+return array;
 }
