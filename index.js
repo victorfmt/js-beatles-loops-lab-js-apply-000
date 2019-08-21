202f3dfba9d27a5c06712ef1dbc69e332f4b10f4
Victor Torres <victor.fmt@gmail.com>
2019-08-21 20:54:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ function theBeatlesPlay(musicians, instruments) {
 }
 return array;
 }
+function johnLennonFacts(array) {
+var i = 0;
+while (i < array.length) {
+array[i] = array[i] + "!!!";
+i++;
+ }
+return array;
+}
